3c4c4851c92eab448936dfdb90491e38cdca48dc
Rachel Aigen <rachel.aigen@gmail.com>
2017-03-26 18:19:55 +0000

    Add subprocess to control elasticsearch and gpio pins
    GPIO pi_piper library removed from driver.rb and moved to
    subprocess.rb.  Is called as background process from driver
    /config HTTP POST exposure. Subprocess also contains elasticsearch
    example code which creates an index if one does not exist called
    'test-project-index' and proceeds to generate 100 random datapoints,
    posted in chronological order to elasticsearch.  This can later be
    visualized in Kibana.

--- driver.rb
+++ driver.rb
@@ -1,14 +1,11 @@
 #!/usr/bin/env ruby
 
 require 'sinatra/base'
-require 'pi_piper'
 require 'json'
 
 class Driver < Sinatra::Base
-
   configure do 
     set :bind, '0.0.0.0'
-    set :pin, PiPiper::Pin.new(:pin => 2, :direction => :out)
   end
 
   get '/' do
@@ -21,16 +18,8 @@ class Driver < Sinatra::Base
 
   post '/config' do
     puts "Received elasticsearchLocation #{params[:elasticsearchLocation]}"
-    # Connect to ES in future and kick off a run here
+    Process.detach( fork{ exec "ruby ./subprocess.rb #{params[:elasticsearchLocation]} &" } )
     erb :monitoring
-  end
-
-  get '/on' do
-    settings.pin.on
-  end
-
-  get '/off' do
-    settings.pin.off
   end
 end
 
