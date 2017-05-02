ff6a8859e781c8055c9d8a2289f132bf08af10fe
Rachel Aigen <rachel.aigen@gmail.com>
2017-05-02 12:45:23 -0400

    Calibration modified

--- calibration_schedular.rb
+++ calibration_schedular.rb
@@ -4,8 +4,10 @@ require 'time'
 require 'elasticsearch'
 require 'pi_piper'
 
+Thread.abort_on_exception = true
+
 #Generating elasticsearch index
-es = Elasticsearch::Client.new url: ARGV[0],
+es = Elasticsearch::Client.new url: ARGV[0]
 index_exists = es.indices.exists index: "motortest-project-index"
 if !index_exists
 puts "Index \"motortest-project-index\" does not exist. Creating..."
@@ -55,7 +57,7 @@ flowrate = Array.new
 flowsensor_on = true
 running = true
 
-(0..0).each do |i |
+(0..0).each do |i|
   sensorthreads[i] = Thread.new do
         while flowsensor_on do
             value = 0
@@ -75,10 +77,11 @@ running = true
               sensor_number: i,
               value: flowrate[i]
                 }
-            sleep(0.25)
+            sleep(0.2)
       end
   end
 end
+
 
 # Calibration loop
 count = 0
@@ -86,50 +89,59 @@ tot = 0
 average = 0
 variance = 0
 stdev = 0
-while true do
+calibrationon=true
+while calibrationon do
 
   reading = flowrate[0]
+  next if reading.nil?
+  next if reading ==0
   # start accumulating an average for proof that
   # we are, in fact, getting consistent flow...
   #
   # we'll use the average to seed our high/low calculations next
-  if reading > 0
-    count += 1
+ # if count >= 0  && count <= 100 
+    until count >= 5000 do
+    if reading >0 
+     count += 1
     tot += reading
     average = tot/count
     variance = (reading-average)**2/count
     stdev = Math.sqrt(variance)
-    puts "Flow began... average at #{average}"
-  else
-    puts "Awaiting flow or sensor read incorrectly..."
-    next
+   # puts "Flow began... average at #{average} count is #{count}"
+
+ # else
+  #  puts "Awaiting flow or sensor read incorrectly..."
+   # next
+ # end
+  if count == 1000
+    puts "Calibrated after 1000 readings...average at #{average} finding systol/diastol cycles..."
   end
-  if count == 100
-    puts "Calibrated after 100 readings... finding systol/diastol cycles..."
-  end
-  if count >= 100 and count < 300
-    # set up some counters to track what the
+  if count >= 1000 and count < 2000
+   # set up some counters to track what the
     # highest throughput (potential systol) and
     # lowest throughput(potential diastol)  we have seen are
-    highest ||= [average]
-    lowest ||= [average]
-
+    highest ||= [average.to_i]
+    lowest ||= [average.to_i]
+    avg_systol = highest.reduce(:+) / highest.size
+    avg_diastol = lowest.reduce(:+) / lowest.size
     # Do the actual comparison per loop maybe make it the average
-    if reading > highest && stdev < 3 && stdev > -3
-      highest << reading
+   
+    if reading > avg_systol && stdev < 3 && stdev > -3
+      highest.push(reading)
       puts "New high of #{highest} found."
-    elsif reading < lowest && stdev <3 && stdev >-3
-      lowest << reading
+    elsif reading < avg_diastol  && stdev <3 && stdev >-3
+      lowest.push(reading)
       puts "New low of #{lowest} found."
     end
   end
-  if count == 300
+  if count == 2000
+  puts "Count is #{count}"
     # Keep the end users busy with some shiny TEXTTT!!!!
     avg_systol = highest.reduce(:+) / highest.size
-    avg_diastol = lowest.reduce(:+) / highest.size
-    puts "Proceeding with presumed systol of #{highest} and presumed diastol of #{lowest}..."
+    avg_diastol = lowest.reduce(:+) / lowest.size
+    puts "Proceeding with presumed systol of #{avg_systol} and presumed diastol of #{avg_diastol}..."
   end
-  if count >=300 and count < 500
+  if count >=2000 && count <4000
     # set up our variables if needed
     systol_duration_total ||= 0
     systol_count ||= 0
@@ -185,7 +197,7 @@ while true do
     puts "Average systol duration at #{systol_duration_total.to_f / systol_count}s" if systol_count > 0
     puts "Average diastol duration at #{diastol_duration_total.to_f / diastol_count}s" if diastol_count > 0
   end
-  if count >=500
+  if count >=4000
     diff_to_systol = (avg_systol - reading).abs
     diff_to_diastol = (avg_diastol - reading).abs
     if diff_to_systol > diff_to_diastol
@@ -206,11 +218,14 @@ while true do
       end
     end
   end
+  
 end
-
+end
+calibrationon = false 
+end 
 
 # Scheduled mode!!!
-while true do
+while !calibrationon do
   if state == :diastol
     puts "Flipping to diastol..."
     sleep (diastol_duration_total.to_f / diastol_count)
